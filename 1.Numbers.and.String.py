3ef93997d1d367913039ca3e1dd6f85bf64b4c19
Rutvik <rutvikpatel29@gmail.com>
2016-02-19 22:34:17 -0500

    Added files via upload

---
+++ 1.Numbers.and.String.py
@@ -0,0 +1,39 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> 3+ 4
+7
+>>> 3 + 5 * 4
+23
+>>> "rutvik"
+'rutvik'
+>>> 'Dappo'
+'Dappo'
+>>> 'I don\'t know'
+"I don't know"
+>>> print("Hi there :)")
+Hi there :)
+>>> print("user\document\newfolder")
+user\document
+ewfolder
+>>> print(r"user\document\newfolder")
+user\document\newfolder
+>>> 
+>>> age = 13
+>>> age + 6
+19
+>>> firstname = "rutvik"
+>>> lastname = "patel"
+>>> firs
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    firs
+NameError: name 'firs' is not defined
+>>> firstname + " " + lastname
+'rutvik patel'
+>>> firstname * 5
+'rutvikrutvikrutvikrutvikrutvik'
+>>> 

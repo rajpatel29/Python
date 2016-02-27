6dc9bbb3c0e4c88e9a5ea887e025478a535bb283
Rutvik <rutvikpatel29@gmail.com>
2016-02-27 09:52:23 -0500

    Added files via upload

---
+++ 11.Dictionary.py
@@ -0,0 +1,30 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+ 
+>>> family = {'dad' : 'mike'  , 'mom' : 'lisa' , 'bro' : 'bucky' }
+>>> family
+{'dad': 'mike', 'bro': 'bucky', 'mom': 'lisa'}
+>>> family['dad']
+'mike'
+
+>>> copy = family.copy()
+>>> copy
+{'dad': 'mike', 'bro': 'bucky', 'mom': 'lisa'}
+>>> copy.clear()
+>>> copy
+{}
+
+>>> family.has_key('dad')
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    family.has_key('dad')
+AttributeError: 'dict' object has no attribute 'has_key'
+
+
+>>> family.values()
+dict_values(['mike', 'bucky', 'lisa'])
+>>> 
---
+++ 13.if.else.py
@@ -0,0 +1,22 @@
+dappo = "zz44"
+if dappo == "xyz" :
+    print('I am xyz')
+elif dappo == 'zzz' :
+    print('I am zzz')
+else :
+    print('I dont know who you are')
+
+print('\n')
+
+thing = "animal444"
+animal = "cat"
+
+if thing == "animal" :
+    if animal == "cat" :
+        print('I am cat')
+    else :
+        print('I am animal')
+
+else :
+    print('I dont know what are you')
+        

94865798a9fb67034474b272c866a93e0186a41c
Rutvik <rutvikpatel29@gmail.com>
2016-02-28 09:03:11 -0500

    Added files via upload

---
+++ 14.Comparision.py
@@ -0,0 +1,37 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+ 
+>>> one = [1,2,3]
+>>> two = [1,2,3]
+>>> one == two
+True
+>>> one is two
+False
+>>> pizza = "pizza"
+
+>>> 's' in pizza
+False
+>>> three = four = [3,4,5]
+>>> three is four
+True
+>>> example = 5
+
+>>> if example > 1 and example< 10:
+	print('value of example is between 1 and 10')
+
+	
+value of example is between 1 and 10
+>>> if example > 1 and example< 4:
+	print('value of example is between 1 and 10')
+
+	
+>>> if example > 1 or example< 4:
+	print('value of example is between 1 and 10')
+
+	
+value of example is between 1 and 10
+>>> 
---
+++ 15.while.and.for.py
@@ -0,0 +1,10 @@
+b = 1
+while b <= 10:
+    print(b)
+    b += 1
+
+
+
+foods = ['bread' , 'milk' , 'banana']
+for food in foods :
+    print("I want " + food )

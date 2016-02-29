05c27b6f00e12ffb6ba938cec8c54d5ba879f230
Rutvik <rutvikpatel29@gmail.com>
2016-02-29 08:22:06 -0500

    Added files via upload

---
+++ 16.Infinite.loop.dictionary.py
@@ -0,0 +1,12 @@
+ages = {'dad' : 40 , 'mom' : 30 , 'lisa' : 20}
+
+
+for item in ages :
+    print(item  , ages[item] )
+
+
+while 1 :
+    name = input("Enter name ")
+    if name == "quit" :
+        break
+    
